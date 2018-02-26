57476ba03de50e4c78a2463dd84511dc0b4ecaf8
mike-hale <michael.hale48@gmail.com>
2018-02-26 10:32:00 -0800

    used lab2 files

--- print_screenv.v
+++ print_screenv.v
@@ -7,54 +7,59 @@ clk
 input RxD, clk;
 output TxD;
 
-reg reset, trans;
-reg [7:0] tx_byte;
-reg [4:0] index;
-wire recvd, is_recv, is_trans, recv_err;
-wire [7:0] rx_byte;
+/*AUTOWIRE*/
+   // Beginning of automatic wires (for undeclared instantiated-module outputs)
+   wire                 tfifo_empty;            // From tfifo_ of uart_fifo.v
+   wire                 tfifo_full;             // From tfifo_ of uart_fifo.v
+   wire [7:0]           tfifo_out;              // From tfifo_ of uart_fifo.v
+   // End of automatics
 
-uart this_uart(.clk(clk),
-               .rst(reset),
-					.rx(RxD),
-					.tx(TxD),
-					.transmit(trans),
-					.tx_byte(tx_byte),
-					.received(recvd),
-					.is_receiving(is_recv),
-					.is_transmitting(is_trans),
-					.recv_error(recv_err));
+   reg [7:0]            tfifo_in;
+   wire                 tx_active;
+   wire                 tfifo_rd;
+   reg                  tfifo_rd_z;
+   reg [seq_dp_width-1:0]  tx_data;
+   reg [3:0]               state;
 
+assign o_tx_busy = (state!=stIdle);
+assign tfifo_rd = ~tfifo_empty & ~tx_active & ~tfifo_rd_z;
 
+   assign tfifo_wr = ~tfifo_full & (state!=stIdle);
+   
+   uart_fifo tfifo_ (// Outputs
+                     .fifo_cnt          (),
+                     .fifo_out          (tfifo_out[7:0]),
+                     .fifo_full         (tfifo_full),
+                     .fifo_empty        (tfifo_empty),
+                     // Inputs
+                     .fifo_in           (tfifo_in[7:0]),
+                     .fifo_rd           (tfifo_rd),
+                     .fifo_wr           (tfifo_wr),
+                     /*AUTOINST*/
+                     // Inputs
+                     .clk               (clk),
+                     .rst               (rst));
 
-initial begin
-  reset = 0;
-  index = 0;
-  tx_byte[0] = string[index];
-  tx_byte[1] = string[index + 1];
-  tx_byte[2] = string[index + 2];
-  tx_byte[3] = string[index + 3];
-  tx_byte[4] = string[index + 4];
-  tx_byte[5] = string[index + 5];
-  tx_byte[6] = string[index + 6];
-  tx_byte[7] = string[index + 7];
-  trans = 1;
-end
+   always @ (posedge clk)
+     if (rst)
+       tfifo_rd_z <= 1'b0;
+     else
+       tfifo_rd_z <= tfifo_rd;
 
-parameter [79:0] string = "ESKEDDDDIITTTTT\n\n\n\n\n";
-
-always @(negedge is_trans) begin
-  if (index == 20)
-    index <= 0;
-  else
-    index <= index + 1;
-  tx_byte[0] <= string[index];
-  tx_byte[1] <= string[index + 1];
-  tx_byte[2] <= string[index + 2];
-  tx_byte[3] <= string[index + 3];
-  tx_byte[4] <= string[index + 4];
-  tx_byte[5] <= string[index + 5];
-  tx_byte[6] <= string[index + 6];
-  tx_byte[7] <= string[index + 7];
-end
+   uart uart_ (// Outputs
+               .received                (o_rx_valid),
+               .rx_byte                 (o_rx_data[7:0]),
+               .is_receiving            (),
+               .is_transmitting         (tx_active),
+               .recv_error              (),
+               .tx                      (o_tx),
+               // Inputs
+               .rx                      (i_rx),
+               .transmit                (tfifo_rd_z),
+               .tx_byte                 (tfifo_out[7:0]),
+               /*AUTOINST*/
+               // Inputs
+               .clk                     (clk),
+               .rst                     (rst));
 
 endmodule
